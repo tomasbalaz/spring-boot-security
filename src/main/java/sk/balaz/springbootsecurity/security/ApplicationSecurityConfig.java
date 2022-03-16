d8a4ff46cefd7d9e71fe22d985642637ee493a20
Tomas Balaz <tomasba@amdocs.com>
2022-03-16 13:04:59 +0100

    Extend remember me to 21 days.

--- src/main/java/sk/balaz/springbootsecurity/security/ApplicationSecurityConfig.java
+++ src/main/java/sk/balaz/springbootsecurity/security/ApplicationSecurityConfig.java
@@ -13,6 +13,8 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import java.util.concurrent.TimeUnit;
+
 import static sk.balaz.springbootsecurity.security.ApplicationUserRole.*;
 
 @Configuration
@@ -41,7 +43,8 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 .loginPage("/login").permitAll()
                 .defaultSuccessUrl("/courses", true)
                 .and()
-                .rememberMe();
+                .rememberMe().tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(21))
+                .key("very-secure-key");
     }
 
     @Override
